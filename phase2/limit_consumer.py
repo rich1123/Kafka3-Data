17e82b4acf8852faa16d989252d59109505373fe
Rich Maiale <richm1123@gmail.com>
2020-04-27 08:26:14 -0400

    limit is running.  I think it's supposed to isolate custid though and this is merely running a total of wth vs dep

--- phase2/limit_consumer.py
+++ phase2/limit_consumer.py
@@ -1,5 +1,11 @@
+import sqlalchemy as sqlalchemy
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+from contextlib import closing
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
+from sqlalchemy import Integer, String, Column, Sequence, create_engine, table, VARCHAR
 
 class XactionConsumer:
     def __init__(self):
@@ -14,12 +20,15 @@ class XactionConsumer:
         # account is kept.
         self.custBalances = {}
         self.limit = -5000
-        self.amt_total = sum(self.amt)
-        self.wth_sum = 0
-        self.dep_sum = 0
+        # self.wth_sum = 0
+        # self.dep_sum = 0
+        self.total = 0
         # THE PROBLEM is every time we re-run the Consumer, ALL our customer
         # data gets lost!
         # add a way to connect to your database here.
+        self.engine = create_engine('postgresql+psycopg2://rich:Coder20!@localhost:5432/kafka',
+                                    encoding='latin1', echo=True)
+        self.conn = self.engine.connect()
 
         #Go back to the readme.
 
@@ -33,20 +42,23 @@ class XactionConsumer:
                 self.custBalances[message['custid']] = 0
             if message['type'] == 'dep':
                 self.custBalances[message['custid']] += message['amt']
-                self.dep_sum += message['amt']
+                self.total += message['amt']
+            if message['type'] == 'wth' and self.total < self.limit:
+                print(f"amt total:{self.total} is less than or equal to the limit of -5000:")
             # if message['type'] == 'wth' and self.custBalances[message['custid']] < self.limit:
             #     print('withdraw limit reach')
             else:
                 self.custBalances[message['custid']] -= message['amt']
-                self.wth_sum += message['amt']
+                self.total -= message['amt']
             # print(self.custBalances)
             # for self.custBalances[message['custid']] in self.custBalances:
             #     if message['amt'] <= self.limit:
             #         print(f"cust. id {message['custid']}: minimum balance exceeded")
-            for key, value in self.custBalances.items():
-                if value <= self.limit:
-                    print(f"amt total:{self.amt_total} is less than or equal to the limit of -5000:")
-                    print(key, value)
+            # for key, value in self.custBalances.items():
+            #     if value <= self.limit:
+            #         print(f"amt total:{value} is less than or equal to the limit of -5000:")
+            #         print(key, value)
+
 
 
 if __name__ == "__main__":
